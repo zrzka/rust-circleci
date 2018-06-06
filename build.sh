edb8d57316fde2293f13aa100ab8d297146dc1ef
Robert Vojta <rvojta@me.com>
2018-06-06 11:24:58 +0200

    Do not push in the build script

--- build.sh
+++ build.sh
@@ -10,6 +10,3 @@ docker build . \
   --build-arg "rust_stable_version=${RUST_STABLE_VERSION}" \
   --build-arg "rust_nightly_version=${RUST_NIGHTLY_VERSION}" \
   --build-arg "clippy_commit_hash=${CLIPPY_COMMIT_HASH}"
-
-docker push zrzka/rust-circleci:latest
-docker push zrzka/rust-circleci:${RUST_VERSION}
